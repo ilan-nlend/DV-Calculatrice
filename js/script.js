f378774bb97079f03a1d2e9506fd6a4d3c1319ee
ilan nlend <ilannlend@gmail.com>
2024-12-17 19:13:23 +0100

    insertion des prompt nombre

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
-var operation =  prompt("Veuiller choisir une opération :\n")
+var operation =  prompt("Veuiller choisir une opération :\n[+,\n -,\n *,\n /, %]")
 var nombre1 = prompt("Veuiller entrz un nombre :")
 var nombre2 = prompt("Veuiller entrz un nombre :")
-var resultat = prompt()
+nombre1 = Number
+nombre2 =Number
+var resultat = prompt("Le résultat de votre opération est")
