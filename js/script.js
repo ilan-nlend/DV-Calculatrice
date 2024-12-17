5e4c722f66435efc433dc22ca3b85a49078cfb5f
ilan nlend <ilannlend@gmail.com>
2024-12-17 19:07:15 +0100

    insertion des prompt nombre

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 
-prompt("Veuiller choisir une opération :\n")
+var operation =  prompt("Veuiller choisir une opération :\n")
+var nombre1 = prompt("Veuiller entrz un nombre :")
+var nombre2 = prompt("Veuiller entrz un nombre :")
+var resultat = prompt()
